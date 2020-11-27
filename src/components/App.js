cb651e94f40cb49e8b7a93d2c4df1a2048d169b9
vaishaliravasoambi <65502403+vaishaliravasoambi@users.noreply.github.com>
2020-11-27 14:34:34 +0530

    change

--- src/components/App.js
+++ src/components/App.js
@@ -3,17 +3,36 @@ import '../styles/App.css';
 
 const App = () => {
   // write your code here 
-
+  const [time, setTime] = React.useState("");
+  const handleEnter = (event) => {
+    if(event.keyCode === 13) {
+      clearInterval(timerID);
+      if(isNaN(event.target.value)) {
+        setTime(0);
+        return;
+      }
+      setTime(parseInt(event.target.value));
+    }
+    return;
+  }
+  const tick = () => {
+    clearInterval(timerID);
+    if(time <= 0) {
+      return;
+    }
+    setTime(time - 1);
+  }
+  let timerID = setInterval(tick, 1000);
   return (
     <div className="wrapper">
       <div id="whole-center">
         <h1>
-          Reverse countdown for<input id="timeCount" onKeyDown={/* callback function */} /> sec.
+          Reverse countdown for<input id="timeCount" onKeyDown={handleEnter} /> sec.
         </h1>
       </div>
-      <div id="current-time">{/* remaining time */}</div>
+      <div id="current-time">{time}</div>
     </div>
-  )
+  );
 }
 
 
